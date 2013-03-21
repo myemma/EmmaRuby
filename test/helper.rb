b7a11f3ca713398573bd5df5f987f1ea5fb4e4a9
Alex Ezell <aezell@gmail.com>
2013-03-21 14:17:04 -0500

    Change case of "Emma" to match
    I think this was missed in a previous commit.

--- test/helper.rb
+++ test/helper.rb
@@ -5,7 +5,7 @@ require 'vcr'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'Emma'
+require 'emma'
 
 VCR.configure do |c|
   c.cassette_library_dir  = 'test/fixtures/vcr_cassettes'
